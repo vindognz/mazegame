057fb2af699c1a50d343b41d03fde5da589e9e17
Vincent Rodley <vindognz@gmail.com>
2025-11-27 14:33:53 +1300

    add A* pathfinding algorithm and escape to cancel animation, prevent input during solving

--- algorithms.py
+++ algorithms.py
@@ -20,7 +20,18 @@ class Algorithm:
     
     def get_neighbours(self, pos):
         x, y = pos
-        return [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]
+        return [
+            (x+1, y), (x-1, y), (x, y+1), (x, y-1),  # cardinal directions
+            # (x+1, y+1), (x-1, y-1), (x+1, y-1), (x-1, y+1)  # diagonals
+        ]
+
+    # FOR KNIGHT
+    # def get_neighbours(self, pos):
+    #     x, y = pos
+    #     return [
+    #         (x+2, y+1), (x+2, y-1), (x-2, y+1), (x-2, y-1),  # horizontal L's
+    #         (x+1, y+2), (x+1, y-2), (x-1, y+2), (x-1, y-2)   # vertical L's
+    #     ]
 
 class BFS(Algorithm):
     def solve(self):
@@ -55,6 +66,55 @@ class BFS(Algorithm):
         
         yield ('no_path', None, [])
 
-# add more algorithms later like:
-# class AStar(Algorithm):
-# class DFS(Algorithm):
+import heapq
+
+class AStar(Algorithm):
+    def solve(self):
+        # priority queue: (f_score, counter, position)
+        counter = 0  # tie-breaker for equal f_scores
+        start_h = self.heuristic(self.start, self.end)
+        heap = [(start_h, counter, self.start)]
+        counter += 1
+        
+        self.visited = {self.start}
+        parent = {self.start: None}
+        g_score = {self.start: 0}
+        
+        while heap:
+            f, _, pos = heapq.heappop(heap)
+            
+            # yield current position being explored
+            yield ('exploring', pos, list(self.visited))
+            
+            if pos == self.end:
+                # reconstruct path
+                path = []
+                current = self.end
+                while current is not None:
+                    path.append(current)
+                    yield ('path', current, path[:])
+                    current = parent[current]
+                path.reverse()
+                yield ('done', None, path)
+                return
+            
+            for neighbour in self.get_neighbours(pos):
+                if self.is_valid(neighbour):
+                    tentative_g = g_score[pos] + 1
+                    
+                    if neighbour not in g_score or tentative_g < g_score[neighbour]:
+                        g_score[neighbour] = tentative_g
+                        f_score = tentative_g + self.heuristic(neighbour, self.end)
+                        heapq.heappush(heap, (f_score, counter, neighbour))
+                        counter += 1
+                        parent[neighbour] = pos
+                        self.visited.add(neighbour)
+        
+        yield ('no_path', None, [])
+    
+    def heuristic(self, pos1, pos2):
+        # manhattan distance for 4-directional
+        return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
+        
+        # euclidean for 8-directional/diagonals
+        # return ((pos1[0] - pos2[0])**2 + (pos1[1] - pos2[1])**2)**0.5
--- maze_editor.py
+++ maze_editor.py
@@ -44,79 +44,110 @@ class MazeEditor:
             
             # keyboard controls
             if event.type == pygame.KEYDOWN:
-                # clear path on any keypress except G
-                if event.key != pygame.K_g:
-                    self.path = []
                 
-                if event.key == pygame.K_s:
-                    self.export_maze()
-                elif event.key == pygame.K_q:  # q = place start
-                    mouse_x, mouse_y = pygame.mouse.get_pos()
-                    grid_x = mouse_x // self.cell_size
-                    grid_y = mouse_y // self.cell_size
-                    if 0 <= grid_x < self.grid_size and 0 <= grid_y < self.grid_size:
-                        if self.maze[grid_y][grid_x] != 3:  # can't be same as end
-                            # remove old start if it exists
-                            for y in range(self.grid_size):
-                                for x in range(self.grid_size):
-                                    if self.maze[y][x] == 2:
-                                        self.maze[y][x] = 0
-                            self.maze[grid_y][grid_x] = 2
-                elif event.key == pygame.K_e:  # e = place end
-                    mouse_x, mouse_y = pygame.mouse.get_pos()
-                    grid_x = mouse_x // self.cell_size
-                    grid_y = mouse_y // self.cell_size
-                    if 0 <= grid_x < self.grid_size and 0 <= grid_y < self.grid_size:
-                        if self.maze[grid_y][grid_x] != 2:  # can't be same as start
-                            # remove old end if it exists
-                            for y in range(self.grid_size):
-                                for x in range(self.grid_size):
-                                    if self.maze[y][x] == 3:
-                                        self.maze[y][x] = 0
-                            self.maze[grid_y][grid_x] = 3
-                elif event.key == pygame.K_g:  # g = solve!
-                    # find start and end in the grid
-                    start = None
-                    end = None
-                    for y in range(self.grid_size):
-                        for x in range(self.grid_size):
-                            if self.maze[y][x] == 2:
-                                start = (x, y)
-                            elif self.maze[y][x] == 3:
-                                end = (x, y)
-                    
-                    # make sure we have both start and end
-                    if start and end:
-                        from algorithms import BFS
-                        solver = BFS(self.maze, start, end)
-                        self.animation_generator = solver.solve()
-                        self.animating = True
-                        self.explored = set()
+                # escape cancels animation
+                if event.key == pygame.K_ESCAPE:
+                    self.animating = False
+                    self.explored = set()
+                    self.path = []
+                    print("Animation cancelled")
+                    continue
+                
+                if not self.animating:
+                    # clear path on any keypress except G
+                    if event.key != pygame.K_g:
                         self.path = []
-                        print("Starting animation...")
-                    else:
-                        print("Need both start and end points!")
-                elif event.key == pygame.K_c:  # c = clear walls
-                    for y in range(self.grid_size):
-                        for x in range(self.grid_size):
-                            if self.maze[y][x] == 1:
-                                self.maze[y][x] = 0
-                elif event.key == pygame.K_l:  # l = load maze
-                    try:
-                        import json
-                        with open('maze_grid.json', 'r') as f:
-                            loaded_grid = json.load(f)
-                            
-                            # make sure it's the right size
-                            if len(loaded_grid) == self.grid_size and len(loaded_grid[0]) == self.grid_size:
-                                self.maze = loaded_grid
-                                print("Maze loaded!")
-                            else:
-                                print(f"Grid size mismatch! Expected {self.grid_size}x{self.grid_size}")
-                    except FileNotFoundError:
-                        print("No saved maze found :(")
-                    except Exception as e:
-                        print(f"Error loading maze: {e}")
+                    
+                    if event.key == pygame.K_s:
+                        self.export_maze()
+                    elif event.key == pygame.K_q:  # q = place start
+                        mouse_x, mouse_y = pygame.mouse.get_pos()
+                        grid_x = mouse_x // self.cell_size
+                        grid_y = mouse_y // self.cell_size
+                        if 0 <= grid_x < self.grid_size and 0 <= grid_y < self.grid_size:
+                            if self.maze[grid_y][grid_x] != 3:  # can't be same as end
+                                # remove old start if it exists
+                                for y in range(self.grid_size):
+                                    for x in range(self.grid_size):
+                                        if self.maze[y][x] == 2:
+                                            self.maze[y][x] = 0
+                                self.maze[grid_y][grid_x] = 2
+                    elif event.key == pygame.K_e:  # e = place end
+                        mouse_x, mouse_y = pygame.mouse.get_pos()
+                        grid_x = mouse_x // self.cell_size
+                        grid_y = mouse_y // self.cell_size
+                        if 0 <= grid_x < self.grid_size and 0 <= grid_y < self.grid_size:
+                            if self.maze[grid_y][grid_x] != 2:  # can't be same as start
+                                # remove old end if it exists
+                                for y in range(self.grid_size):
+                                    for x in range(self.grid_size):
+                                        if self.maze[y][x] == 3:
+                                            self.maze[y][x] = 0
+                                self.maze[grid_y][grid_x] = 3
+                    elif event.key == pygame.K_g:  # g = solve!
+                        # find start and end in the grid
+                        start = None
+                        end = None
+                        for y in range(self.grid_size):
+                            for x in range(self.grid_size):
+                                if self.maze[y][x] == 2:
+                                    start = (x, y)
+                                elif self.maze[y][x] == 3:
+                                    end = (x, y)
+                        
+                        # make sure we have both start and end
+                        if start and end:
+                            from algorithms import BFS
+                            solver = BFS(self.maze, start, end)
+                            self.animation_generator = solver.solve()
+                            self.animating = True
+                            self.explored = set()
+                            self.path = []
+                            print("Starting animation...")
+                        else:
+                            print("Need both start and end points!")
+                    elif event.key == pygame.K_c:  # c = clear walls
+                        for y in range(self.grid_size):
+                            for x in range(self.grid_size):
+                                if self.maze[y][x] == 1:
+                                    self.maze[y][x] = 0
+                    elif event.key == pygame.K_l:  # l = load maze
+                        try:
+                            import json
+                            with open('maze_grid.json', 'r') as f:
+                                loaded_grid = json.load(f)
+                                
+                                # make sure it's the right size
+                                if len(loaded_grid) == self.grid_size and len(loaded_grid[0]) == self.grid_size:
+                                    self.maze = loaded_grid
+                                    print("Maze loaded!")
+                                else:
+                                    print(f"Grid size mismatch! Expected {self.grid_size}x{self.grid_size}")
+                        except FileNotFoundError:
+                            print("No saved maze found :(")
+                        except Exception as e:
+                            print(f"Error loading maze: {e}")
+                    elif event.key == pygame.K_h:  # h = solve with A*
+                        # find start and end in the grid
+                        start = None
+                        end = None
+                        for y in range(self.grid_size):
+                            for x in range(self.grid_size):
+                                if self.maze[y][x] == 2:
+                                    start = (x, y)
+                                elif self.maze[y][x] == 3:
+                                    end = (x, y)
+                        
+                        if start and end:
+                            from algorithms import AStar
+                            solver = AStar(self.maze, start, end)
+                            self.animation_generator = solver.solve()
+                            self.animating = True
+                            self.explored = set()
+                            self.path = []
+                            print("Starting A* animation...")
+                        else:
+                            print("Need both start and end points!")
         
         # mouse drawing
         mouse_pressed = pygame.mouse.get_pressed()
